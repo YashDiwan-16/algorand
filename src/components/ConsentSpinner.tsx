d9d904f656660c0a64144a08d0f9768583a12723
Sarthak Nimje <sarthaknimje@gmail.com>
2025-06-30 22:03:24 +0530

    add loading spinner component

---
+++ src/components/ConsentSpinner.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ConsentSpinner: React.FC = () => (
+  <span className="inline-block w-5 h-5 border-2 border-gray-300 border-t-blue-500 rounded-full animate-spin"></span>
+);
+
+export default ConsentSpinner; 
