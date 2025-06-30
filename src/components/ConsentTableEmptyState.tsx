ffa0844d7ea4c508c10e7df6513cce3eea5643db
Sarthak Nimje <sarthaknimje@gmail.com>
2025-06-30 20:26:56 +0530

    add empty state for consent table

---
+++ src/components/ConsentTableEmptyState.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ConsentTableEmptyState: React.FC = () => (
+  <div className="text-center text-gray-400 py-8">
+    <p>No consents found.</p>
+  </div>
+);
+
+export default ConsentTableEmptyState; 
